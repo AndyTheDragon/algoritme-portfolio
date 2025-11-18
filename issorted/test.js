d3f554fdb02522dd0ae282b939013dd9e376961d
AndyTheDragon <andre.jansam@gmail.com>
2025-11-18 13:15:10 +0100

    refactor tests for isSorted function to use Mocha framework

--- issorted/test.js
+++ issorted/test.js
@@ -1,9 +1,38 @@
+import assert from "assert";
 import isSorted from "./issorted.js";
 
-console.log(`Array [1, 2, 3, 4, 5, 6, 7, 8, 9] is sorted: ${isSorted([1, 2, 3, 4, 5, 6, 7, 8, 9])}`); // true
-console.log(`Array [1, 2, 4, 5, 7, 9, 11] is sorted: ${isSorted([1, 2, 4, 5, 7, 9, 11])}`); // true
-console.log(`Array [2, 2, 3, 6, 8, 9, 9, 10, 10, 11, 12] is sorted: ${isSorted([2, 2, 3, 6, 8, 9, 9, 10, 10, 11, 12])}`); // true
-console.log(`Array [9, 8, 7, 6, 5, 4, 3, 2, 1] is sorted: ${isSorted([9, 8, 7, 6, 5, 4, 3, 2, 1])}`); // false
-console.log(`Array [1, 8, 9, 4, 6, 7, 2, 5, 3] is sorted: ${isSorted([1, 8, 9, 4, 6, 7, 2, 5, 3])}`); // false
-console.log(`Array [1, 2, 4, 4, 5, 3, 6, 7, 8] is sorted: ${isSorted([1, 2, 4, 4, 5, 3, 6, 7, 8])}`); // false
-console.log(`Array [4, 4, 4, 4, 4, 4, 4] is sorted: ${isSorted([4, 4, 4, 4, 4, 4, 4])}`); // true
+describe("isSorted", function () {
+
+  it("returns true for [1,2,3,4,5,6,7,8,9]", function () {
+    assert.strictEqual(isSorted([1,2,3,4,5,6,7,8,9]), true);
+  });
+
+  it("returns true for [1,2,4,5,7,9,11]", function () {
+    assert.strictEqual(isSorted([1,2,4,5,7,9,11]), true);
+  });
+
+  it("returns true for [2,2,3,6,8,9,9,10,10,11,12]", function () {
+    assert.strictEqual(isSorted([2,2,3,6,8,9,9,10,10,11,12]), true);
+  });
+
+  it("returns false for [9,8,7,6,5,4,3,2,1]", function () {
+    assert.strictEqual(isSorted([9,8,7,6,5,4,3,2,1]), false);
+  });
+
+  it("returns false for [1,8,9,4,6,7,2,5,3]", function () {
+    assert.strictEqual(isSorted([1,8,9,4,6,7,2,5,3]), false);
+  });
+
+  it("returns false for [1,2,4,4,5,3,6,7,8]", function () {
+    assert.strictEqual(isSorted([1,2,4,4,5,3,6,7,8]), false);
+  });
+
+  it("returns true for [4,4,4,4,4,4,4]", function () {
+    assert.strictEqual(isSorted([4,4,4,4,4,4,4]), true);
+  });
+
+  it("returns false for [2,3,5,4]", function () {
+    assert.strictEqual(isSorted([2,3,5,4]), false);
+  });
+
+});
